9dc525e41acc480fc87cf9bc893cfa447b335210
romi0330 <romina03villavicencio@gmail.com>
2023-06-06 12:49:18 -0500

    impresion de calificaciones

--- examen.c
+++ examen.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main (int argc, char *argv[]) {
-    float suma, promedio;
-    int alumnos[23];
+    srand(time(NULL));
+    int suma, promedio;
+    int alumnos[24][4];
+
+    for (int i = 1; i <= 23; i++)
+    {
+        suma=0;
+        for (int j = 1; j <= 3; j++)
+        {
+    //en el rand se pone un rango maximo de ingreso de estudiantes de 300
+            alumnos[i][j]=rand()%10;
+            suma+=alumnos[i][j];
+        
+    }
+    }
+    for (int i = 1; i <= 23; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            printf("Estudiante #%d. Progreso #%d Calificacion=%d \n", i, j, alumnos[i][j]);
+        }
+
+    }
     return 0;
 }
