39ef45473e66b5f5fce0b433de84b90259632d32
romi0330 <romina03villavicencio@gmail.com>
2023-06-06 13:22:04 -0500

    no acabe

--- examen.c
+++ examen.c
@@ -4,25 +4,29 @@
 
 int main (int argc, char *argv[]) {
     srand(time(NULL));
-    int suma, promedio, alu=0, p1=0,p2,p3;
+    int suma, alu=0, p1=0,p2,p3, pro=0;
+    int promedio;
     int alumnos[24][4];
 
     for (int i = 1; i <= 23; i++)
     {
+        pro=suma/3;
         suma=0;
         for (int j = 1; j <= 3; j++)
         {
     //en el rand se pone un rango maximo de ingreso de estudiantes de 300
             alumnos[i][j]=rand()%10;
             suma+=alumnos[i][j];
+            
         }
+        
         if (suma>alu)
         {
             alu=suma;
+            
             p1=i;
         }
-        
-        
+        promedio=alu/3;
         
     }
     
@@ -31,10 +35,11 @@ int main (int argc, char *argv[]) {
         for (int j = 1; j <= 3; j++)
         {
             printf("Estudiante #%d. Progreso #%d Calificacion=%d \n", i, j, alumnos[i][j]);
+ 
         }
-
+                   printf("El promedio del estudiante %d es %d \n", i, pro);
     }
-    printf("El alumno con mejor promedio en todo el semestre es el #%d", p1);
+    printf("El alumno con mejor promedio en todo el semestre es el #%d con %d de promedio", p1, promedio);
    
     return 0;
 }
